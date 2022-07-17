e11ed94ba9ee1e6422d94d360f32c327d7b83e2d
HristinaY <hristina.yovcheva@gmail.com>
2022-07-17 22:33:55 +0100

    carousel js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("log")
+$(".carousel-arr").on("click", function() {
+    const carouselItems = $(".carousel-item");
+    let currentItem = $(".carousel-item.active");
+    let nextItem;
+
+    if ($(this).hasClass("arr-back")) {
+        nextItem = $(currentItem).prev(".carousel-item");
+        if ($(nextItem).length == 0) {
+            nextItem = $(carouselItems).last();
+        }
+    }
+    else {
+        nextItem = $(currentItem).next(".carousel-item");
+        if ($(nextItem).length == 0) {
+            nextItem = $(carouselItems).first();
+        }
+    }
+    $(nextItem).addClass("active");
+    $(currentItem).removeClass("active");
+});
+
+
